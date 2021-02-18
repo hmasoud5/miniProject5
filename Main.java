088ce477ccd79330be676ce504df8ac049df2544
Heba Masoud <heba.masoud@patelhs.org>
2021-02-18 15:39:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    // recieving user input from the user
+    System.out.println("Welcome! What is your name?");
+    // Welcoming patients
+    String name = scan.next();
+    // saving and recieving name into type string
+    checkIn(name);
+
+  }
+  //method called checkIn
+  static void checkIn(String name) 
+  {
+    String[] names = new String[5];
+  }
+
+}
