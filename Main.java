d7cd5716d66ea6def2ddbd83a573ce5a533a76e5
Heba Masoud <heba.masoud@patelhs.org>
2021-02-22 13:28:17 +0000

    method called back into

--- Main.java
+++ Main.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     // recieving user input from the user
     System.out.println("Welcome! What is your name?");
     // Welcoming patients
     String name = scan.next();
     // saving and recieving name into type string
-    checkIn(name);
+    int index = 0;
+    checkIn(name, index);
+    
 
   }
-  //method called checkIn 
-  static void checkIn(String name) 
+
+  // method called checkIn
+  static boolean checkIn(String name, int index) 
   {
-    String[] names = new String[5];
-    //need base
-  }
-
+    String[] names = {"Bella", "Journey", "Neveah"};
+    if (name = names[index]){
+     System.out.println("Yay! You're checked in. ");
+     return true;
+    }else{
+      index++;
+      checkIn(name, index);
+    } 
+    if(index < names.length){
+      index = 1; 
+      checkIn(name, index); 
+    } 
+}
 }
